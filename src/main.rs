0deb9338b7fe7d952536ec01a510bc78d43d0239
Ilya Tepikin <ilya.tepikin@gmail.com>
2023-04-19 21:31:56 +0300

    Add ip v6 support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::net::{Ipv4Addr, SocketAddrV4};
+use std::net::{Ipv4Addr, SocketAddrV4, Ipv6Addr, SocketAddrV6};
 use std::str::FromStr;
 use std::sync::Arc;
 use network_interface::{NetworkInterface, NetworkInterfaceConfig};
@@ -7,6 +7,7 @@ use tokio::net::UdpSocket;
 use tokio::time::{sleep, Duration};
 
 const MULTICAST_GROUP: Ipv4Addr = Ipv4Addr::new(239, 15, 16, 17);
+const MULTICAST_GROUP_V6: Ipv6Addr = Ipv6Addr::new(0xff00, 0, 0, 0, 0, 0, 0, 0);
 const MULTICAST_PORT: u16 = 55100;
 
 #[tokio::main]
@@ -17,13 +18,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let network_interfaces = NetworkInterface::show().unwrap();
 
     for itf in network_interfaces.iter() {
+        println!("{}: {:?}", itf.name, itf);
         for v in itf.addr.iter() {
-            if v.ip().is_ipv4() && !v.ip().is_loopback() {
-                let socket = UdpSocket::bind(format!("{}:{}", v.ip(), MULTICAST_PORT)).await?;
+            if !v.ip().is_loopback() {
+                let socket = UdpSocket::bind(format!("{}:{}", v.ip(), MULTICAST_PORT)).await.expect("Could not bind socket");
                 println!("Listening on {}", socket.local_addr()?);
                 socket.set_broadcast(true)?;
-                socket.set_multicast_ttl_v4(1)?;
-                socket.join_multicast_v4(MULTICAST_GROUP, Ipv4Addr::from_str(v.ip().to_string().as_str()).unwrap())?;
+                if v.ip().is_ipv4() {
+                    socket.set_multicast_loop_v4(true)?;
+                    socket.set_multicast_ttl_v4(1)?;
+                    socket.join_multicast_v4(MULTICAST_GROUP, Ipv4Addr::from_str(v.ip().to_string().as_str()).unwrap())?;
+                } else {
+                    socket.set_multicast_loop_v6(true)?;
+                    socket.join_multicast_v6(&MULTICAST_GROUP_V6, 0)?;
+                }
 
                 iface_map.insert(v.ip().to_string(), socket);
             }
@@ -55,10 +63,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         let data = format!("DIS:");
-        let addr = SocketAddrV4::new(MULTICAST_GROUP, MULTICAST_PORT);
+        let addr_v4 = SocketAddrV4::new(MULTICAST_GROUP, MULTICAST_PORT);
+        let addr_v6 = SocketAddrV6::new(MULTICAST_GROUP_V6, MULTICAST_PORT, 0, 0);
         for (key, value) in s2.iter() {
             println!("sending from: {}", key);
-            let _ = value.send_to(data.as_bytes(), &addr).await;
+            if value.local_addr().unwrap().is_ipv4() {
+                let _ = value.send_to(data.as_bytes(), addr_v4).await;
+            } else {
+                let _ = value.send_to(data.as_bytes(), addr_v6).await;
+            }
         }
         sleep(Duration::from_secs(1)).await;
     }
